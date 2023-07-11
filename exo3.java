47a738953ef942d1f7c94da85cf796756e238bf5
thibault <thibault.moirand@loatys.com>
2023-07-11 17:17:40 +0200

    17h17/11/07

--- exo3.java
+++ exo3.java
@@ -7,15 +7,32 @@ public class exo3 {
     public static void main(String[] args){
         int min = 0;
         int max = 10000;
+        int i = 0;
+        int nb = 0;
+        Scanner donner = new Scanner(System.in);
         int randomNb = (int) Math.floor(Math.random() * (max - min + 1) + min);
         System.out.println(randomNb);
+        while (i < 1) {
 
-        Scanner donner = new Scanner(System.in);
-        System.out.println("Veuillez saisir un chiffre entre "+min+" et "+max+" :");
-        int chiffre1 = donner.nextInt();
-        System.out.println("Vous avez saisi : " + chiffre1);
-        if (chiffre1 == randomNb){
-            System.out.println("gg");
+            System.out.println("Veuillez saisir un chiffre entre " + min + " et " + max + " :");
+            int chiffre1 = donner.nextInt();
+//            System.out.println("Vous avez saisi : " + chiffre1);
+            nb++;
+            if (chiffre1 == randomNb) {
+                System.out.println("gg");
+                i++;
+                System.out.println("tu a "+nb+" tentative");
+            }
+            if (chiffre1 < randomNb){
+                System.out.println("plus");
+            }
+            if (chiffre1 > randomNb){
+                System.out.println("moin");
+            }
+            long start = System.currentTimeMillis();
+// ...
+            long finish = System.currentTimeMillis();
+            long timeElapsed = finish - start;
         }
 
 
